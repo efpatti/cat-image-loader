b2ee5c6562e3662d7f9470b9196746ba82cece6a
efpatti <enzo.patti@aluno.senai.br>
2024-02-24 23:33:07 -0300

    check

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [imageCat, setImageCat] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!isLoading) {
+        searchCatImage();
+      }
+    }, 3000); // Troca de imagem a cada 3 segundos
+
+    return () => clearInterval(interval);
+  }, [isLoading]); // Chama useEffect somente quando isLoading muda
+
+  const searchCatImage = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get(
+        "https://api.thecatapi.com/v1/images/search"
+      );
+      const catImageURL = response.data[0].url;
+      setImageCat(catImageURL);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen flex justify-center items-center">
+      {imageCat && (
+        <div
+          className="h-screen w-screen bg-cover bg-center"
+          style={{ backgroundImage: `url(${imageCat})` }}
+        />
+      )}
+      {error && <p className="text-red-500">Ocorreu um erro: {error}</p>}
     </div>
   );
-}
+};
 
 export default App;
